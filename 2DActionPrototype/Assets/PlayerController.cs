3d4feed4b1ecfbf352510c85222ab3b472811ca0
COLOR <zyaooo99@yahoo.co.jp>
2018-12-05 12:30:34 +0900

    ジャンプを１回だけに制限、速度でジャンプ力が変化する様に。調整はまだ雑

--- 2DActionPrototype/Assets/PlayerController.cs
+++ 2DActionPrototype/Assets/PlayerController.cs
@@ -6,14 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
 
-    float JumpForce = 500.0f; //ジャンプのベクトルを収納する関数
-    float MoveForce = 10.0f; //横移動のベクトルを収納する関数
-    float MoveLimit = 5.0f; //横移動時のベクトル上限を収納する関数
-    float DashForce = 20.0f; //ダッシュ時のベクトルを収納する関数
-    float DashLimit = 10.0f; //ダッシュ時のベクトル上限を収納する関数
+    float JumpForce = 0.0f; //ジャンプのベクトルを収納する
+    float JumpLowest = 1000.0f; //ジャンプのベクトルの下限を収納する
+    float MoveForce = 30.0f; //横移動のベクトルを収納する
+    float MoveLimit = 5.0f; //横移動時のベクトル上限を収納する
+    float DashForce = 50.0f; //ダッシュ時のベクトルを収納する
+    float DashLimit = 10.0f; //ダッシュ時のベクトル上限を収納する
 
-    float PlayerForce = 0.0f; //プレイヤーのX軸ベクトルを収納する関数
-    float PlayerLimit = 0.0f; //プレイヤーのX軸ベクトル上限を収納する関数
+    float PlayerForce = 0.0f; //プレイヤーのX軸ベクトルを収納する
+    float PlayerLimit = 0.0f; //プレイヤーのX軸ベクトル上限を収納する
+
+    bool JumpOnGround = false; //プレイヤーのジャンプを１回に制御する
+    float JumpVelocity = 0.0f; //ジャンプした瞬間の速度を収納する
 
     void Start()
     {
@@ -24,22 +28,42 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //「Zキーが押された時」にジャンプ
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && this.JumpOnGround)
         {
-            this.rigid2D.AddForce(transform.up * this.JumpForce);
+            //ジャンプした時の速度を収納
+            this.JumpVelocity = Mathf.Floor(this.rigid2D.velocity.x); //Floorで少数端数をカット
+
+            //助走がついた時にジャンプ力を増す様に
+            if (this.JumpVelocity > 5.0f && Input.GetKey(KeyCode.X))
+            {
+                this.JumpForce = (this.JumpVelocity - 5.0f) * 50.0f + JumpLowest;
+            }
+            else if(this.JumpVelocity < -5.0f && Input.GetKey(KeyCode.X))
+            {
+                this.JumpForce = (this.JumpVelocity * (-1) - 5.0f) * 50.0f + JumpLowest;
+            }
+            else
+            {
+                this.JumpForce = this.JumpLowest; //ダッシュ時以外のジャンプ力はデフォルトに統一
+            }
+
+            this.rigid2D.AddForce(transform.up * this.JumpForce); //ジャンプ力の適応
+            this.JumpOnGround = false; //地面から離れたことを収納
         }
 
         //「十字右キーが押されている時」に右へ移動
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if(this.rigid2D.velocity.x < PlayerLimit)
+            if (this.rigid2D.velocity.x < this.PlayerLimit)
+            {
                 this.rigid2D.AddForce(transform.right * this.PlayerForce);
+            }
         }
 
         //「十字左キーが押されている時」に左へ移動
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (this.rigid2D.velocity.x > PlayerLimit * -1)
+            if (this.rigid2D.velocity.x > this.PlayerLimit * -1)
                 this.rigid2D.AddForce(transform.right * this.PlayerForce * -1);
         }
 
@@ -53,6 +77,16 @@ public class PlayerController : MonoBehaviour
         {
             this.PlayerForce = this.MoveForce;
             this.PlayerLimit = this.MoveLimit;
+        }
+    }
+
+    //ジャンプを１回に制御する為に地面衝突を判定する関数
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //タグ「Ground」で地面に触れた時という制限を追加
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            this.JumpOnGround = true;
         }
     }
 }
