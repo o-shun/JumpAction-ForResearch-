87d76d8496af5f9d9fa14a48d543d36f88b4f5dd
COLOR <zyaooo99@yahoo.co.jp>
2018-12-08 17:24:47 +0900

    物理マテリアル着地時の反発をゼロに。あと急ブレーキを実装

--- 2DActionPrototype/Assets/PlayerController.cs
+++ 2DActionPrototype/Assets/PlayerController.cs
@@ -8,9 +8,9 @@ public class PlayerController : MonoBehaviour
 
     float JumpForce = 0.0f; //ジャンプのベクトルを収納する
     float JumpLowest = 1000.0f; //ジャンプのベクトルの下限を収納する
-    float MoveForce = 30.0f; //横移動のベクトルを収納する
+    float MoveForce = 20.0f; //横移動のベクトルを収納する
     float MoveLimit = 5.0f; //横移動時のベクトル上限を収納する
-    float DashForce = 50.0f; //ダッシュ時のベクトルを収納する
+    float DashForce = 25.0f; //ダッシュ時のベクトルを収納する
     float DashLimit = 10.0f; //ダッシュ時のベクトル上限を収納する
 
     float PlayerForce = 0.0f; //プレイヤーのX軸ベクトルを収納する
@@ -32,43 +32,63 @@ public class PlayerController : MonoBehaviour
         {
             //ジャンプした時の速度を収納
             this.JumpVelocity = Mathf.Floor(this.rigid2D.velocity.x); //Floorで少数端数をカット
+            //ジャンプの最大値の調整
+            if (this.JumpVelocity > 10.0f) 
+            {
+                this.JumpVelocity = 10.0f;
+            }
 
             //助走がついた時にジャンプ力を増す様に
-            if (this.JumpVelocity > 5.0f && Input.GetKey(KeyCode.X))
+            if (this.JumpVelocity > 5.0f && Input.GetKey(KeyCode.X)) //右向きの時
             {
                 this.JumpForce = (this.JumpVelocity - 5.0f) * 50.0f + JumpLowest;
             }
-            else if(this.JumpVelocity < -5.0f && Input.GetKey(KeyCode.X))
+            else if(this.JumpVelocity < -5.0f && Input.GetKey(KeyCode.X)) //左向きの時
             {
                 this.JumpForce = (this.JumpVelocity * (-1) - 5.0f) * 50.0f + JumpLowest;
             }
             else
             {
-                this.JumpForce = this.JumpLowest; //ダッシュ時以外のジャンプ力はデフォルトに統一
+                this.JumpForce = this.JumpLowest; //ダッシュ時以外のジャンプ力は統一
             }
 
             this.rigid2D.AddForce(transform.up * this.JumpForce); //ジャンプ力の適応
             this.JumpOnGround = false; //地面から離れたことを収納
         }
 
-        //「十字右キーが押されている時」に右へ移動
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        //横移動
+        if (Input.GetKey(KeyCode.RightArrow)) //「十字右キーが押されている時」に右へ移動
         {
+            //速度制限をつける
             if (this.rigid2D.velocity.x < this.PlayerLimit)
             {
                 this.rigid2D.AddForce(transform.right * this.PlayerForce);
             }
         }
-
-        //「十字左キーが押されている時」に左へ移動
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow)) //「十字左キーが押されている時」に左へ移動
         {
+            //速度制限をつける
             if (this.rigid2D.velocity.x > this.PlayerLimit * -1)
+            {
                 this.rigid2D.AddForce(transform.right * this.PlayerForce * -1);
+            }
+        }
+        else //ブレーキ
+        {
+            if (this.rigid2D.velocity.x > 0.0f && this.JumpOnGround) //右向きの時
+            {
+                rigid2D.velocity = Vector2.zero;
+            }
+            else if (this.rigid2D.velocity.x < 0.0f && this.JumpOnGround) //左向きの時
+            {
+                rigid2D.velocity = Vector2.zero;
+            }
         }
 
-        //「Xキーが押されている時」にダッシュ
-        if (Input.GetKey(KeyCode.X))
+
+        //プレイヤーの速度と最高速を状況判断で決定
+        if (Input.GetKey(KeyCode.X)) //「Xキーが押されている時」にダッシュ
         {
             this.PlayerForce = this.DashForce;
             this.PlayerLimit = this.DashLimit;
